7dbafb2dfac373f5c4825295b8e658da648c926e
sd_exercise <sd_exercise@naist-wavenet124-105.naist.jp>
2014-01-15 19:04:41 +0900

    created single-view project

--- grasphy/grasphy/ViewController.h
+++ grasphy/grasphy/ViewController.h
@@ -1,5 +1,5 @@
 //
-//  AppDelegate.h
+//  ViewController.h
 //  grasphy
 //
 //  Created by sd_exercise on 2014/01/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
-
-@property (strong, nonatomic) UIWindow *window;
+@interface ViewController : UIViewController
 
 @end
